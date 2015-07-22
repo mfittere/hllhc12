f6cada3bd4adae50c8a5cee49f41bf82658ce2a2
mfittere <miriam.fitterer@cern.ch>
2015-07-22 21:05:09 +0200

    fixed bug in toolkit/check_mcb.py which did not calculate the maximum over the corrector strenght right

--- toolkit/check_mcb.py
+++ toolkit/check_mcb.py
@@ -22,6 +22,15 @@ iterror.update({'acby%ss4'%(pp): 0.0 for pp in ['h','v']})
 iterror.update({'acby%ss5'%(pp): 0.0 for pp in ['h','v']})
 iterror.update({'acbc%s%s'%(pp,ii): 0.0 for ii in ['6','7'] for pp in ['h','v']})
 
+#dictionary with strength required for lumi scan knobs
+lumi= {'acbx%s1'%(pp): 0.0 for pp in ['h','v']}
+lumi.update({'acbx%s2'%(pp): 0.0 for pp in ['h','v']})
+lumi.update({'acbx%s3'%(pp): 0.0 for pp in ['h','v']})
+lumi.update({'acbrd%s4'%(pp): 0.27 for pp in ['h','v']})
+lumi.update({'acby%ss4'%(pp): 0.20 for pp in ['h','v']})
+lumi.update({'acby%ss5'%(pp): 0.0 for pp in ['h','v']})
+lumi.update({'acbc%s%s'%(pp,ii): 0.0 for ii in ['6','7'] for pp in ['h','v']})
+
 def mk_dic(fn): 
   """create dictionary from mad output file"""
   corrs={}
@@ -39,19 +48,23 @@ def print_acb(corrs,name,scale = 23348.89927):
   tot=0
   for pp in ['x','s','o','ccp','ccm','ccs']:
     out.append(scale*corrs[name+pp])
-  #iterror
+  #iterror+lumiscan
   out.append(iterror[name.split('.')[0]])
-  #calculate total strength
+  out.append(lumi[name.split('.')[0]])
+  #--calculate total strength
   tot = scale*max(abs(corrs[name+'x']),abs(corrs[name+'s']))
+  #add contribution from other knobs
   for pp in ['o','ccp','ccm','ccs']:
     tot=tot+scale*abs(corrs[name+pp])
-  tot = tot+iterror[name.split('.')[0]]#add strength necessary for IT error correction
+  tot = tot+abs(iterror[name.split('.')[0]])#add strength necessary for IT error correction
+  tot = tot+abs(lumi[name.split('.')[0]])
   out.append(tot)
   #maximum and margin
   cmax=corrmax[name.split('.')[0]]
   out.append(cmax)
-  out.append(abs(cmax-tot)*100/cmax)
-  print ('%-15s'+'%7.2f'*10) % tuple(out)
+  margin=abs(cmax-tot)*100/cmax
+  out.append(margin)
+  print ('%-15s'+'%7.2f'*11) % tuple(out)
   return out
 
 def check_orbit(fn):
@@ -59,7 +72,7 @@ def check_orbit(fn):
   print "for 'tot' use maximum value over 'x' and 's'"
   corrs=mk_dic(fn)
   print('%50s') % ('corrector strength [Tm]')
-  print('%-18s'+' %-6s'*10) % ('name','x','s','o','ccp','ccm','ccs','iterr','tot','max','margin [%]')
+  print('%-18s'+' %-6s'*11) % ('name','x','s','o','ccp','ccm','ccs','iterr','lumi','tot','max','margin [%]')
   outall=[]#list to save the maximum values in for the summary table
   for cc,ii in zip(['acbx','acbx','acbx','acbrd','acby','acby','acbc','acbc'],['1','2','3','4','s4','s5','6','7']): 
     outallmax=[]
@@ -76,9 +89,12 @@ def check_orbit(fn):
             name='%s%s%s.%s5%s'%(cc,pp,ii,ss,bb)
             if name+'x' in corrs.keys():
               out=print_acb(corrs,name)
-              outallmax.append(out[1:-1])#maximum strength
+              outallmax.append([abs(ll) for ll in out[1:-1] ])#maximum strength
               outmargin.append([out[-1]])
-    sout=(('-- '+name.split('.')[0]).ljust(15)+'%7.2f'*10) % tuple(max(outallmax)+min(outmargin))
+    #get the maximum,use numpy method in order to define the axis
+    maxall=list(np.max(np.array(outallmax),axis=0))
+    marginall=[np.min(np.array(outmargin))]
+    sout=(('-- '+name.split('.')[0]).ljust(15)+'%7.2f'*11) % tuple(maxall+min(outmargin))
     outall.append([sout])
     print sout
   print 'Summary table'
