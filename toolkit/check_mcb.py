1157c36182601e8472f7ee3a71196477e384de87
Riccardo De Maria <riccardodemaria@gmail.com>
2015-07-23 21:01:14 +0200

    merged with miriam

--- toolkit/check_mcb.py
+++ toolkit/check_mcb.py
@@ -22,6 +22,35 @@ iterror.update({'acby%ss4'%(pp): 0.0 for pp in ['h','v']})
 iterror.update({'acby%ss5'%(pp): 0.0 for pp in ['h','v']})
 iterror.update({'acbc%s%s'%(pp,ii): 0.0 for ii in ['6','7'] for pp in ['h','v']})
 
+#dictionary with strength required for lumi scan knobs
+lumi= {'acbx%s1'%(pp): 0.0 for pp in ['h','v']}
+lumi.update({'acbx%s2'%(pp): 0.0 for pp in ['h','v']})
+lumi.update({'acbx%s3'%(pp): 0.0 for pp in ['h','v']})
+lumi.update({'acbrd%s4'%(pp): 0.27 for pp in ['h','v']})
+lumi.update({'acby%ss4'%(pp): 0.20 for pp in ['h','v']})
+lumi.update({'acby%ss5'%(pp): 0.0 for pp in ['h','v']})
+lumi.update({'acbc%s%s'%(pp,ii): 0.0 for ii in ['6','7'] for pp in ['h','v']})
+
+#dictionary with strength required for correction of arc imperfections scan knobs
+arc= {'acbx%s1'%(pp): 0.0 for pp in ['h','v']}
+arc.update({'acbx%s2'%(pp): 0.0 for pp in ['h','v']})
+arc.update({'acbx%s3'%(pp): 0.0 for pp in ['h','v']})
+arc.update({'acbrd%s4'%(pp): 0.35 for pp in ['h','v']})
+arc.update({'acby%ss4'%(pp): 0.35 for pp in ['h','v']})
+arc.update({'acby%ss5'%(pp): 0.0 for pp in ['h','v']})
+arc.update({'acbc%s%s'%(pp,ii): 0.0 for ii in ['6','7'] for pp in ['h','v']})
+
+#dictionary with strength required for D2 transfer fucntion errors scan knobs
+#assume 1.e-3 transfer function error for 35 Tm
+d2err=35*2.e-3
+d2trans= {'acbx%s1'%(pp): 0.0 for pp in ['h','v']}
+d2trans.update({'acbx%s2'%(pp): 0.0 for pp in ['h','v']})
+d2trans.update({'acbx%s3'%(pp): 0.0 for pp in ['h','v']})
+d2trans.update({'acbrd%s4'%(pp): d2err for pp in ['h','v']})
+d2trans.update({'acby%ss4'%(pp): 0.0 for pp in ['h','v']})
+d2trans.update({'acby%ss5'%(pp): 0.0 for pp in ['h','v']})
+d2trans.update({'acbc%s%s'%(pp,ii): 0.0 for ii in ['6','7'] for pp in ['h','v']})
+
 def mk_dic(fn): 
   """create dictionary from mad output file"""
   corrs={}
@@ -39,19 +68,23 @@ def print_acb(corrs,name,scale = 23348.89927):
   tot=0
   for pp in ['x','s','o','ccp','ccm','ccs']:
     out.append(scale*corrs[name+pp])
-  #iterror
-  out.append(iterror[name.split('.')[0]])
-  #calculate total strength
+  #iterror+lumiscan
+  for err in iterror,lumi,arc,d2trans:
+    out.append(err[name.split('.')[0]])
+  #--calculate total strength
   tot = scale*max(abs(corrs[name+'x']),abs(corrs[name+'s']))
+  #add contribution from other knobs
   for pp in ['o','ccp','ccm','ccs']:
     tot=tot+scale*abs(corrs[name+pp])
-  tot = tot+iterror[name.split('.')[0]]#add strength necessary for IT error correction
+  for err in iterror,lumi,arc,d2trans:
+    tot = tot+abs(err[name.split('.')[0]])#add strength necessary for IT error correction
   out.append(tot)
   #maximum and margin
   cmax=corrmax[name.split('.')[0]]
   out.append(cmax)
-  out.append(abs(cmax-tot)*100/cmax)
-  print ('%-15s'+'%7.2f'*10) % tuple(out)
+  margin=(cmax-tot)*100/cmax
+  out.append(margin)
+  print ('%-15s'+'%7.2f'*13) % tuple(out)
   return out
 
 def check_orbit(fn):
@@ -59,7 +92,7 @@ def check_orbit(fn):
   print "for 'tot' use maximum value over 'x' and 's'"
   corrs=mk_dic(fn)
   print('%50s') % ('corrector strength [Tm]')
-  print('%-18s'+' %-6s'*10) % ('name','x','s','o','ccp','ccm','ccs','iterr','tot','max','margin [%]')
+  print('%-18s'+' %-6s'*13) % ('name','x','s','o','ccp','ccm','ccs','iterr','lumi','arc','d2trans','tot','max','margin [%]')
   outall=[]#list to save the maximum values in for the summary table
   for cc,ii in zip(['acbx','acbx','acbx','acbrd','acby','acby','acbc','acbc'],['1','2','3','4','s4','s5','6','7']): 
     outallmax=[]
@@ -76,14 +109,17 @@ def check_orbit(fn):
             name='%s%s%s.%s5%s'%(cc,pp,ii,ss,bb)
             if name+'x' in corrs.keys():
               out=print_acb(corrs,name)
-              outallmax.append(out[1:-1])#maximum strength
+              outallmax.append([abs(ll) for ll in out[1:-1] ])#maximum strength
               outmargin.append([out[-1]])
-    sout=(('-- '+name.split('.')[0]).ljust(15)+'%7.2f'*10) % tuple(max(outallmax)+min(outmargin))
+    #get the maximum,use numpy method in order to define the axis
+    maxall=list(np.max(np.array(outallmax),axis=0))
+    marginall=[np.min(np.array(outmargin))]
+    sout=(('-- '+name.split('.')[0].replace('v','[hv]')).ljust(15)+'%7.2f'*13) % tuple(maxall+min(outmargin))
     outall.append([sout])
     print sout
   print 'Summary table'
   print('%50s') % ('corrector strength [Tm]')
-  print('%-18s'+' %-6s'*10) % ('name','x','s','o','ccp','ccm','ccs','iterr','tot','max','margin [%]')
+  print('%-18s'+' %-6s'*13) % ('name','x','s','o','ccp','ccm','ccs','iterror','lumi','arc','d2trans','tot','max','margin [%]')
   for ll in outall:
     print '%s'%(ll[0])
 
