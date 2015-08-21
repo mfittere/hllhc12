scan results with corrected cc voltage calculation

study_presqueeze/analysispkg.py
@@ -17,7 +17,7 @@ def from_acfa(t,ldrift):
   bxrb2=t.betx_acfarb2_ref-2*t.alfx_acfarb2_ref*ldrift+ldrift**2*(1+t.alfx_acfarb2_ref**2)/t.betx_acfarb2_ref
   byrb2=t.bety_acfarb2_ref-2*t.alfy_acfarb2_ref*ldrift+ldrift**2*(1+t.alfy_acfarb2_ref**2)/t.bety_acfarb2_ref
   return bxlb1,bylb1,bxrb2,byrb2
-def get_cc(t,ll='r',bb='2'):
+def get_cc(t,ll='r',bb='2',xy='x'):
   """calculate the cc voltage *vcrab* in [MV]:
      vcrab = ncc*Ebeam*tan(xing)*sigma_s/(beta*sin(2*pi*hrf_cc*sigma_s/clight))*1.e-6
   ncc   : number of cc per IP and side (e.g. lb2)
@@ -35,7 +35,7 @@ def get_cc(t,ll='r',bb='2'):
   Ebeam=7.0e12 #V
   sigma_s=0.075 #bunch length [m]
   ncc=4#number of crab cavities
-  beta  = np.sum(np.array([ sqrt(beta_star*t['betx_acf%s%sb%s_ref'%(aa,ll,bb)]) for aa in 'abcd']),axis=0)
+  beta  = np.sum(np.array([ sqrt(beta_star*t['bet%s_acf%s%sb%s_ref'%(xy,aa,ll,bb)]) for aa in 'abcd']),axis=0)
   vcrab = ncc*Ebeam*tan(xing)*sigma_s/(beta*sin(2*pi*hrf_cc*sigma_s/clight))*1.e-6
   return vcrab
 def get_cc_max(t):
@@ -45,15 +45,15 @@ def get_cc_max(t):
   it is sufficient to take the max over one beam
   and one ir"""
   if 'v_crabv.r1b1' in t.keys():
-    aa=aa='v_crabh.l1b1,v_crabh.r1b1,v_crabh.l5b1,v_crabh.r5b1,v_crabh.l1b2,v_crabh.r1b2,v_crabh.l5b2,v_crabh.r5b2,v_crabv.l1b1,v_crabv.r1b1,v_crabv.l5b1,v_crabv.r5b1,v_crabv.l1b2,v_crabv.r1b2,v_crabv.l5b2,v_crabv.r5b2'.split(',')
+    aa='v_crabh.l1b1,v_crabh.r1b1,v_crabh.l5b1,v_crabh.r5b1,v_crabh.l1b2,v_crabh.r1b2,v_crabh.l5b2,v_crabh.r5b2,v_crabv.l1b1,v_crabv.r1b1,v_crabv.l5b1,v_crabv.r5b1,v_crabv.l1b2,v_crabv.r1b2,v_crabv.l5b2,v_crabv.r5b2'.split(',')
     t['vcrabh_lb2_ref'] = t['v_crabh.l5b2']
     t['vcrabh_rb2_ref'] = t['v_crabh.r5b2']
     vmax=np.max([ t[kk] for kk in aa ],axis=0)
   else:
-    t['vcrabh_lb2_ref'] = get_cc(t,ll='l',bb='2')
-    t['vcrabh_rb2_ref'] = get_cc(t,ll='r',bb='2')
-    aa=np.array([ t['vcrabh_lb2_ref'],t['vcrabh_rb2_ref']])
-    vmax=aa.max(axis=0)
+    t['vcrabh_lb2_ref'] = get_cc(t,ll='l',bb='2',xy='x')
+    t['vcrabh_rb2_ref'] = get_cc(t,ll='r',bb='2',xy='x')
+    vcrab = np.array([ get_cc(t,ll,bb,xy) for ll in 'lr' for bb in '12' for xy in 'xy' ])
+    vmax=vcrab.max(axis=0)
   return t,vmax
 
 def get_sum_beta(t):

study_presqueeze/mkplot.py
@@ -44,21 +44,29 @@ def plot_range(t,lpar):
   pmax=max([max(t[ll]) for ll in lpar])
   pmin=min([min(t[ll]) for ll in lpar])
   return (pmin-20,pmax+100)
-def mkfig_crab_wire(fn):
+def mkfig_crab_wire(fn,indfig=False):
+    """create all plots in one big figure
+    indfig = False: one figure
+    indfig = True : each subplot in a individual figure"""
     ynn='g1 g2 g3'.split()
     t=optics.open(fn)
     fnbase=fn.split('.tfs')[0]
     case=fnbase.split('/')[0].split('scan')[1]
+    if(indfig == True): figname=fnbase+'_cc'+case
+    else: figname=(fnbase+'_cc'+case+'.png')
     brho7TeV=23348.89927
     qtlimq5= 200.0#Q5
     qtlim3 = 200.0#Q7
     #calculate cc voltage
     t,vmax=get_cc_max(t)#adds also t['vcrabh_lb2_ref'] and t['vcrabh_rb2_ref'] to t
     #--- figure 1
-    figure(fn+'_cc',figsize=(20,12))
-    title(fnbase+'_cc')
-    clf()
-    subplot(341)
+    if(indfig == True):
+      figure()
+    else:
+      figure(fn+'_cc',figsize=(20,12))
+      title(fnbase+'_cc')
+      clf()
+      subplot(341)
     for yy in ynn:
         plot(t.betx_acfdrb2_ref,abs(t[yy]),'.',label=yy)
         plot(t.betx_acfdrb2_ref[0],abs(t[yy])[0],'ko')#starting point
@@ -69,7 +77,10 @@ def mkfig_crab_wire(fn):
     ylim(128,133)
     legend()
     grid()
-    subplot(342)
+    if(indfig == True):
+      savefig(figname+'_1.png')
+      figure() 
+    else: subplot(342)
     for yy in ynn:
         plot(t.bety_acfdrb2_ref,abs(t[yy]),'.',label=yy)
         plot(t.bety_acfdrb2_ref[0],abs(t[yy])[0],'ko')#starting point
@@ -80,7 +91,10 @@ def mkfig_crab_wire(fn):
     ylim(128,133)
     legend()
     grid()
-    subplot(343)
+    if(indfig == True):
+      savefig(figname+'_2.png')
+      figure()
+    else: subplot(343)
     for cc in 'abcd':
       for ll in 'lr':
         pp='_acf%s%sb2_ref'%(cc,ll)
@@ -93,7 +107,10 @@ def mkfig_crab_wire(fn):
     ylim(600,1800)
     legend()
     grid()
-    subplot(344)
+    if(indfig == True):
+      savefig(figname+'_3.png')
+      figure()
+    else: subplot(344)
     #calculate beta at wire at 7m
     t['betx_wire7mlb1_ref'],t['bety_wire7mlb1_ref'],t['betx_wire7mrb2_ref'],t['bety_wire7mrb2_ref']=from_wire_5m(t,2)
     for pw in '357':
@@ -113,12 +130,16 @@ def mkfig_crab_wire(fn):
 #    xlim(plot_range(t,['betx_wire3mlb1_ref','betx_wire5mlb1_ref','betx_wire7mlb1_ref','betx_wire3mrb2_ref','betx_wire5mrb2_ref','betx_wire7mrb2_ref']))
 #    ylim(plot_range(t,['bety_wire3mlb1_ref','bety_wire5mlb1_ref','bety_wire7mlb1_ref','bety_wire3mrb2_ref','bety_wire5mrb2_ref','bety_wire7mrb2_ref']))
     #---- Vmax vs betx/bety wire
-    subplot(345)
+    if(indfig == True):
+      savefig(figname+'_4.png')
+      figure()
+    else: subplot(345)
     #calculate beta at wire at 7m
     t['betx_wire7mlb1_ref'],t['bety_wire7mlb1_ref'],t['betx_wire7mrb2_ref'],t['bety_wire7mrb2_ref']=from_wire_5m(t,2)
     for pw in '357':
       plot(vmax,t['betx_wire'+pw+'mrb2_ref']/t['bety_wire'+pw+'mrb2_ref'],'.',label='Wire '+pw+'m RB2')
       plot(vmax[0],t['betx_wire'+pw+'mrb2_ref'][0]/t['bety_wire'+pw+'mrb2_ref'][0],'ko')#starting point
+    plot([0,20],[2.0,2.0],'k-')
     xlabel(r'$V_{\rm crab, max} \ [MV]$')
     ylabel(r'$\beta_x/\beta_y(\rm wire)$')
     legend(loc='upper left')
@@ -126,7 +147,10 @@ def mkfig_crab_wire(fn):
     xlim(11,12.5)
     ylim(1.4,4.0)
     #---- Vmax vs beta_min(MCBYY), beta_max(MCBYY)
-    subplot(346)
+    if(indfig == True):
+      savefig(figname+'_5.png')
+      figure()
+    else: subplot(346)
     bmin_mcbyy=np.min([t.betx_mcby_ref,t.bety_mcby_ref], axis=0)
     bmax_mcbyy=np.max([t.betx_mcby_ref,t.bety_mcby_ref], axis=0)
     plot(vmax,bmin_mcbyy,'.',label='min(beta(MCBYY))')
@@ -140,7 +164,10 @@ def mkfig_crab_wire(fn):
     ylim(300,1600)
     grid()
     #---- Vmax vs impedance
-    subplot(347)
+    if(indfig == True):
+      savefig(figname+'_6.png')
+      figure()
+    else: subplot(347)
     beta_crab_sum = get_sum_beta(t)
     plot(vmax,beta_crab_sum*1.e-3,'.',label='LB2+RB2')
     plot(vmax[0],beta_crab_sum[0]*1.e-3,'ko')#starting point
@@ -151,7 +178,10 @@ def mkfig_crab_wire(fn):
     ylim(17,22)
     grid()
     #---- Vmax vs kq5
-    subplot(348)
+    if(indfig == True):
+      savefig(figname+'_7.png')
+      figure()
+    else: subplot(348)
     for ss in 'lr':
       for bb in '12':
         plot(vmax,abs(t['kq5.%s5b%s'%(ss,bb)])*brho7TeV,'.',label='kq5.%s5b%s'%(ss,bb)) 
@@ -165,7 +195,10 @@ def mkfig_crab_wire(fn):
     grid()
     legend(loc='upper left') 
     #---- Vmax vs kq7 strength
-    subplot(349)
+    if(indfig == True):
+      savefig(figname+'_8.png')
+      figure()
+    else: subplot(349)
     for ss in 'lr':
       for bb in '12':
         plot(vmax,abs(t['kq7.%s5b%s'%(ss,bb)])*brho7TeV,'.',label='kq7.%s5b%s'%(ss,bb)) 
@@ -179,7 +212,10 @@ def mkfig_crab_wire(fn):
     grid()
     legend(loc='center right')
     #---- imq5l vs kq7 strength
-    subplot(3,4,10)
+    if(indfig == True):
+      savefig(figname+'_9.png')
+      figure()
+    else: subplot(3,4,10)
     imq5l=t['kq5.l5b2']/t['kq5.l5b1']
     for ss in 'lr':
       for bb in '12':
@@ -195,7 +231,10 @@ def mkfig_crab_wire(fn):
     grid()
     legend(loc='center right')
     #---- imq5r vs kq7 strength
-    subplot(3,4,11)
+    if(indfig == True):
+      savefig(figname+'_10.png')
+      figure()
+    else: subplot(3,4,11)
     imq5r=t['kq5.r5b2']/t['kq5.r5b1']
     for ss in 'lr':
       for bb in '12':
@@ -210,9 +249,11 @@ def mkfig_crab_wire(fn):
     ylabel(r'B [T/m]')
     grid()
     legend(loc='center left')
-    tight_layout()
-    figname=(fnbase+'_cc'+case+'.png')
-    savefig(figname)
+    if(indfig == True):
+      savefig(figname+'_11.png')
+    else:
+      tight_layout()
+      savefig(figname)
 close('all')
 #mkfig('results_riccardo/presqueze_q4_scan99.3.tfs')
 #mkfig('results_riccardo/presqueze_q4_scan100.tfs')
@@ -220,17 +261,18 @@ close('all')
 #mkfig('scan_q4_8m/presqueze_q4_scan99.3.tfs')
 #mkfig('scan_q4_8m/presqueze_q4_scan100.tfs')
 #mkfig('scan_q4_8m/presqueze_q4_scan107.tfs')
+indfig=False
 #mkfig_crab_wire('scan_q4_8m/presqueze_q4_scan100.tfs')
 #mkfig_crab_wire('scan_q4_8m/presqueze_q4_scan107.tfs')
-mkfig_crab_wire('scan_q4_8m/presqueze_q4_scan99.3.tfs')
-#mkfig_crab_wire('scan_q4_10m/presqueze_q4_scan99.3.tfs')
-#mkfig_crab_wire('scan_q4_6m/presqueze_q4_scan99.3.tfs')
-#mkfig_crab_wire('scan_q5_9m/presqueze_q4_scan99.3.tfs')
-#mkfig_crab_wire('scan_q5_13m/presqueze_q4_scan99.3.tfs')
-#mkfig_crab_wire('scan_q4_6m_q5_9m/presqueze_q4_scan99.3.tfs')
-#mkfig_crab_wire('scan_q4_6m_q5_13m/presqueze_q4_scan99.3.tfs')
-#mkfig_crab_wire('scan_q4_10m_q5_9m/presqueze_q4_scan99.3.tfs')
-#mkfig_crab_wire('scan_q4_10m_q5_13m/presqueze_q4_scan99.3.tfs')
+mkfig_crab_wire('scan_q4_8m/presqueze_q4_scan99.3.tfs',indfig)
+mkfig_crab_wire('scan_q4_10m/presqueze_q4_scan99.3.tfs')
+mkfig_crab_wire('scan_q4_6m/presqueze_q4_scan99.3.tfs')
+mkfig_crab_wire('scan_q5_9m/presqueze_q4_scan99.3.tfs')
+mkfig_crab_wire('scan_q5_13m/presqueze_q4_scan99.3.tfs')
+mkfig_crab_wire('scan_q4_6m_q5_9m/presqueze_q4_scan99.3.tfs')
+mkfig_crab_wire('scan_q4_6m_q5_13m/presqueze_q4_scan99.3.tfs')
+mkfig_crab_wire('scan_q4_10m_q5_9m/presqueze_q4_scan99.3.tfs')
+mkfig_crab_wire('scan_q4_10m_q5_13m/presqueze_q4_scan99.3.tfs')
 
 draw()
 show()